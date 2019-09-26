b06408b08f2192ee04b582904301811ab48cbafd
= <=>
2019-09-26 16:46:07 +0700

    thien ha

---
+++ chuyen-doi-tien-te.js
@@ -0,0 +1,17 @@
+function hay() {
+    let a= parseInt(document.getElementById('num1').value)
+    let b= parseInt(document.getElementById('from').value)
+    let c= parseInt(document.getElementById('to').value)
+    let d = a*c/b
+    alert('result :' + d)
+
+    //document.getElementById('3').innerHTML = "result :"+d
+   //document.write('result :'+d)
+        // document.getElementById('idamount').focus()
+
+
+
+}
+
+
+    
